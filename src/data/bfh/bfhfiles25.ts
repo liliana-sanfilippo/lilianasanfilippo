17152fb7d2386f6987736321b79a7ae7abaf8271
liliana-sanfilippo <liliana-sanfilippo@users.noreply.github.com>
2026-05-24 14:17:34 +0000

    deploy: 27bf4a9bb3a216197660f57d0cf272b4e632561f

--- src/data/bfh/bfhfiles25.ts
+++
@@ -1,9 +0,0 @@
-export const bfhfiles25 = [
-    {
-        id: 1,
-        title: "Programm",
-        thumbnail: "https://static.igem.wiki/teams/5833/other/otherimages/bfh24-programm.avif",
-
-        pdf: "",
-    }
-]
--- src/data/imgLinks.ts
+++
@@ -1,7 +0,0 @@
-export const newspaperImg = "https://www.svgrepo.com/show/260121/newspaper-news.svg";
-export const radioImg = "https://www.svgrepo.com/show/266362/play-pause.svg";
-export const tvImg = "https://www.svgrepo.com/show/260137/television.svg";
-export const blogImg = "https://www.svgrepo.com/show/343850/blog-seo-optimization-search.svg";
-export const magazineImg = "https://www.svgrepo.com/show/181756/open-book-book.svg"
-export const linkedInImg = "https://www.svgrepo.com/show/448234/linkedin.svg"
-export const webImg = "https://www.svgrepo.com/show/415803/website-ui-web.svg"
